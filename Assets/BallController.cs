ae61046ab3bee45aa3e7ec7a5a6c24c78b99c1c4
Varun Patel <rvjpatel99@gmail.com>
2025-01-28 22:14:02 -0800

    added jump mechanic using space bar and gameplay recording

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -5,39 +5,15 @@ public class BallBehaviour : MonoBehaviour
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2.0f;
 
-    public void MoveBall(Vector2 input)
+    public void MoveBall(Vector3 input)
     {
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        Vector3 inputXZPlane = new(input.x, input.y, input.z);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero; // intialize our input vector
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
-        }
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-
-        Debug.Log("Resultant Vector: " + inputVector);
-
-        Vector3 inputXZPlane = new(inputVector.x, 0, inputVector.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,30 +3,35 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
 
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero; // intialize our input vector
+        Vector3 inputVector = Vector3.zero; // intialize our input vector
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
+            inputVector += Vector3.forward; // "a += b" <=> "a = a + b"
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            inputVector += Vector2.down;
+            inputVector += Vector3.back;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector += Vector2.right;
+            inputVector += Vector3.right;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            inputVector += Vector2.left;
+            inputVector += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            inputVector += Vector3.up + new Vector3(0f, 1f, 0f);
         }
 
         OnMove?.Invoke(inputVector);
